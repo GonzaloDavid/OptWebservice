47f0a51e6d09a46a677ee09a987d51c09a9cac14
Gonzalo David Proaño <binproag@baninter.com>
2025-08-19 21:14:49 -0500

    Primer ws

---
+++ OptWebservice/src/main/java/WebService/JAXRSConfiguration.java
@@ -0,0 +1,15 @@
+package WebService;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        resources.add(PrimerosPasosWs.class);
+        return resources;
+    }
+}
--- OptWebservice/src/main/java/WebService/PrimerosPasosWs.java
+++ OptWebservice/src/main/java/WebService/PrimerosPasosWs.java
@@ -1,9 +1,23 @@
 package WebService;
 
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Stateless
+@Path("primerosPasos")
 public class PrimerosPasosWs {
-
-
-
     public PrimerosPasosWs() {
+    }
+
+    @GET
+    @Path("getHellowWold")
+    @Produces({ MediaType.APPLICATION_JSON})
+    public String getHellowWold()
+    {
+        return "Hola Mundo";
     }
 }
