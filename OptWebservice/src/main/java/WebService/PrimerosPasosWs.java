d3e9d30df828bbb91cd30a410bffb76f4f2b91fa
luiswiwi <luiswiwi1@hotmail.com>
2025-09-01 20:52:35 -0500

    pruebas mensaje

--- OptWebservice/src/main/java/WebService/PrimerosPasosWs.java
+++ OptWebservice/src/main/java/WebService/PrimerosPasosWs.java
@@ -36,20 +36,27 @@ public class PrimerosPasosWs {
     public List<Cliente> guardaCliente(List<Cliente> inputListaClientes)
     {
         List<Cliente> listaRetorno=new ArrayList<>();
-        for(Cliente clienteIteracion: inputListaClientes)
-        {
-            String nombreMayusculas=client.convertirNombresMayuscula(clienteIteracion.getNombre());
-            String codvalidaIdentificacion=client.validaIdentificacion(clienteIteracion.getIdentificacion());
-            String tipoiden=client.VerifiIden(clienteIteracion.getTipoIdentificacion());
-            String edad=client.validarEdad(clienteIteracion.getFechaNacimiento());
-            String nombre=client.validarNombre(clienteIteracion.getNombre());
-            clienteIteracion.setCodEdad(edad);
-            clienteIteracion.setCodNombre(nombre);
-            clienteIteracion.setNombre(nombreMayusculas);
-            clienteIteracion.setCodRetornoIdentificacion(codvalidaIdentificacion);
-            clienteIteracion.setCodtipoIdentificacion(tipoiden);
-            listaRetorno.add(clienteIteracion);
+        try{
+
+            for(Cliente clienteIteracion: inputListaClientes)
+            {
+                String nombreMayusculas=client.convertirNombresMayuscula(clienteIteracion.getNombre());
+                String codvalidaIdentificacion=client.validaIdentificacion(clienteIteracion.getIdentificacion());
+                String tipoiden=client.VerifiIden(clienteIteracion.getTipoIdentificacion());
+                String edad=client.validarEdad(clienteIteracion.getFechaNacimiento());
+                String nombre=client.validarNombre(clienteIteracion.getNombre());
+                clienteIteracion.setCodEdad(edad);
+                clienteIteracion.setCodNombre(nombre);
+                clienteIteracion.setNombre(nombreMayusculas);
+                clienteIteracion.setCodRetornoIdentificacion(codvalidaIdentificacion);
+                clienteIteracion.setCodtipoIdentificacion(tipoiden);
+                listaRetorno.add(clienteIteracion);
+            }
         }
+        catch (Exception error){
+            System.out.println("Ocurrio un error: " + error.getMessage());
+        }
+
         return listaRetorno;
 
     }
