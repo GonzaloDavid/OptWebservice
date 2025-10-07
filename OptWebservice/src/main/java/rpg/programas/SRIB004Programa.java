166d28c7a06919e0109fd6c8efe6c01eb80d220b
luiswiwi <luiswiwi1@hotmail.com>
2025-10-06 21:28:14 -0500

    prospecto

---
+++ OptWebservice/src/main/java/rpg/programas/SRIB004Programa.java
@@ -0,0 +1,69 @@
+package rpg.programas;
+
+import dto.RespuestaMensajejt;
+
+import ec.com.bancointernacional.SRIB004.INP_CABECERA;
+import ec.com.bancointernacional.SRIB004.CLI_DATPROS;
+import ec.com.bancointernacional.SRIB004.DS_INPDATCLI;
+import rpg.conexion.Util;
+
+import javax.ejb.Stateless;
+import java.math.BigDecimal;
+@Stateless
+public class SRIB004Programa {
+
+    public ec.com.bancointernacional.SRIB004.INP_CABECERA setupHeader(String user) {
+        ec.com.bancointernacional.SRIB004.INP_CABECERA header = new ec.com.bancointernacional.SRIB004.INP_CABECERA();
+        header.setADITIONALCUSID("");
+        header.setBANKID("01");
+        header.setCHANNEL("OPT");
+        header.setCUSTOMERID("1");
+        header.setGROUPID("");
+        header.setIPADDRESS("");
+        header.setPRODUCTCODE("1");
+        header.setDATEANDTIME("2025-10-06 10:04:10.581");
+        header.setSEQUENTIAL(new BigDecimal(1));
+        header.setUSERID(user);
+        header.setSERVICECODE("20004");
+        header.setTRANSERVICECODE("2066");
+        return header;
+    }
+    public RespuestaMensajejt datoProspectoProgramas(String user, BigDecimal bgNumpro, BigDecimal bgCodcli)
+    {
+        RespuestaMensajejt response=new RespuestaMensajejt();
+        try{
+            //Creamos el objeto cabecera
+            INP_CABECERA header=setupHeader(user);
+
+            //Creamos objeto input
+            DS_INPDATCLI d=new DS_INPDATCLI();
+            d.setCUSTNROPRO(new BigDecimal(bgNumpro.doubleValue()));
+            d.setCUSTCODCLI (new BigDecimal(bgCodcli.doubleValue()));
+
+            CLI_DATPROS programAS400 = new CLI_DATPROS();
+            programAS400.setIN_CABEC(header);
+            programAS400.setIN_DATCLI(d);
+            Util.invokeTrx(programAS400);
+
+            if (programAS400.getReturnValue() != 0 || "".equals(programAS400.getOU_CABEC().getMESSAGEDESCR().trim())) {
+
+                System.out.println("Resultado programa as400"+ programAS400.getOU_CABEC().getMESSAGEDESCR());
+                response.setCodigo("00");
+                response.setMensaje(programAS400.getOU_CABEC().getMESSAGEDESCR());
+
+            } else {
+
+                response.setCodigo("500");
+                System.out.println("Resultado con error "+ programAS400.getOU_CABEC().getMESSAGEDESCR());
+                response.setMensaje(programAS400.getOU_CABEC().getMESSAGEDESCR());
+            }
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return response;
+    }
+}
+
+
